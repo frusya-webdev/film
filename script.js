d828ffaf9578e237eb4193a39da95a70be2a00e7
Frusya <shapovalova.tatyana91@gmail.com>
2022-08-17 01:00:55 +0200

    Added a condition to check input from the user and a condition to compare the rating

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const numberOfFilms = +prompt('How many films have you already watched?', '')
+const numberOfFilms = prompt('How many films have you already watched?', '')
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -8,11 +8,61 @@ const personalMovieDB = {
     privat: false,
 }
 
-const getFilmTitle1 = prompt('What\'s the last movie you watched?', ''),
-      getFilmRating1 = prompt('How do you rate it?', ''),
-      getFilmTitle2 = prompt('What\'s the last movie you watched?', ''),
-      getFilmRating2 = prompt('How do you rate it?', '')
+for(let i = 0; i < 2; i++) {
+    const getFilmTitle = prompt('What\'s the last movie you watched?', ''),
+        getFilmRating = prompt('How do you rate it?', '');
 
-personalMovieDB.movies[getFilmTitle1] = getFilmRating1 //  имя_обьекта.свойство_обьекта[ключ] = значение
-personalMovieDB.movies[getFilmTitle2] = getFilmRating2 // когда записіваем какие-то свойства в обьект, лучше через [] а не через .
+    if(getFilmTitle != null && getFilmRating != null && getFilmTitle != '' && getFilmRating != '' && getFilmTitle.length < 50) {
+        personalMovieDB.movies[getFilmTitle] = getFilmRating //  имя_обьекта.свойство_обьекта[ключ] = значение. когда записіваем какие-то свойства в обьект, лучше через [] а не через .
+        console.log('done')
+    }
+    else {
+        console.log('error')
+        i--;
+    }
+}
+
+/*альтернатива
+let i = 0;
+do {
+    const getFilmTitle = prompt('What\'s the last movie you watched?', ''),
+          getFilmRating = prompt('How do you rate it?', '');
+
+    if (getFilmTitle != null && getFilmRating != null && getFilmTitle != '' && getFilmRating != '' && getFilmTitle.length < 50) {
+        personalMovieDB.movies[getFilmTitle] = getFilmRating //  имя_обьекта.свойство_обьекта[ключ] = значение. когда записіваем какие-то свойства в обьект, лучше через [] а не через .
+        console.log('done')
+    } else {
+        console.log('error')
+        i--;
+    }
+    i++;
+}
+while(i < 2)*/
+
+/*альтернатива
+let i = 0;
+while(i < 2) {
+    const getFilmTitle = prompt('What\'s the last movie you watched?', ''),
+          getFilmRating = prompt('How do you rate it?', '');
+
+    if (getFilmTitle != null && getFilmRating != null && getFilmTitle != '' && getFilmRating != '' && getFilmTitle.length < 50) {
+        personalMovieDB.movies[getFilmTitle] = getFilmRating //  имя_обьекта.свойство_обьекта[ключ] = значение. когда записіваем какие-то свойства в обьект, лучше через [] а не через .
+        console.log('done')
+    } else {
+        console.log('error')
+        i--;
+    }
+    i++;
+}*/
+
+if(personalMovieDB.count <= 10) {
+    console.log('Watched quite a few films')
+} else if (personalMovieDB.count > 10 && personalMovieDB.count <= 30) {
+    console.log('You are a classic viewer')
+} else if (personalMovieDB.count > 30) {
+    console.log('You are a cinephile!')
+} else {
+    console.log('Error')
+}
+
 console.log(personalMovieDB)
